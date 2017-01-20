3c1e1f3789fe29f074f891bc851f6bc8fb01c37e
Andrew Apperley <andrew.apperley@shopify.com>
2017-01-19 23:59:01 -0500

    Update resume.swift

--- resume.swift
+++ resume.swift
@@ -10,7 +10,7 @@ class AndrewApperley: Human, Husband, IOSDevelopment, BackendDevelopment, Applic
 
   var _summary: String
   var _education: Dictionary<String, String>
-  var _career = Array<Dictionary<String, String>>
+  var _career = Array<Dictionary<String, Any>>
   var _interests = Array<String>
   var _notableProjects = Array<Dictionary<String, String>>
   
@@ -33,13 +33,61 @@ class AndrewApperley: Human, Husband, IOSDevelopment, BackendDevelopment, Applic
     return [
       "type": "College",
       "name": "Humber College",
-      "timeFrame": "2010 - 2012",
+      "timeframe": "2010 - 2012",
       "program": "Multimedia Design and Production Technician"
     ]
   }
   
-  func career() -> Array<Dictionary<String, String>> {
-    
+  func career() -> Array<Dictionary<String, Any>> {
+    return [
+      [
+        "role": "Mobile Developer".
+        "company": "Shopify",
+        "timeframe": "December 2016 - Present (2 months)"
+      ],
+      
+      [
+        "role": "Part Time Instructor".
+        "company": "Humber College",
+        "timeframe": "September 2016 - December (4 months)"
+      ],
+      
+      [
+        "role": "Senior iOS Applications Developer".
+        "company": "Tiny Hearts",
+        "timeframe": "June 2016 - December 2016 (7 months)"
+      ],
+      
+      [
+        "role": "Senior Server Developer".
+        "company": "Quiver Media Inc.",
+        "timeframe": "October 2015 - June 2016 (9 months)"
+      ],
+      
+      [
+        "role": "Full Stack Developer".
+        "company": "Publicis",
+        "timeframe": "January 2015 - October 2015 (9 months)"
+      ],
+       
+      [
+        "role": "Application Developer".
+        "company": "BitBlitz Apps Inc.",
+        "timeframe": "April 2014 - December 2014 (9 months)"
+      ],
+      
+      [
+        "role": "Interactive Developer".
+        "company": "Digiflare",
+        "timeframe": "September 2013 - April 2014 (8 months)"
+      ],
+      
+      [
+        "role": "Junior Flash/iOS Developer".
+        "company": "DesignAxiom Ltd.",
+        "timeframe": "December 2011 - April 2013 (1 year 5 months)"
+      ]
+    ]
   }
   
   func interests() -> Array<String> {
